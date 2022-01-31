7db2fbe053b2ae42cc6c9a9fe2835bd7c3d862a9
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-01-31 16:52:27 +0300

    Added project example005_condition

---
+++ example005_condition/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username == "Masha")
+{
+    Console.Writeline("Hooray! It's Masha!");
+}
+else
+{
+    Console.Write("Hi, .");
+    Console.WriteLine(username);
+}
+
