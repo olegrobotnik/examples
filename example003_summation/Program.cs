a8c51db1081d5e5863487385d34e51629e97f03f
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-01-31 16:42:12 +0300

    Added project example003_summation

---
+++ example003_summation/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 
+int numberOne = 3;
+int numberTwo = 5;
+Console.WriteLine(numberOne + numberTwo); 
+*/
+//Another representation of the code without intermediate variable
+
+/*
+int numberOne = 3;
+int numberTwo = 5;
+int sum = numberOne + numberTwo;
+Console.WriteLine(sum);
+*/
+//Using sum variable to display
+
+int numberOne = new Random().Next(1, 10); //1 2 3 4 5 6 7 8 9 
+Console.WriteLine(numberOne);
+int numberTwo = new Random().Next(1, 10);
+Console.WriteLine(numberTwo);
+int sum = numberOne + numberTwo;
+Console.WriteLine(sum);
+//Using random
