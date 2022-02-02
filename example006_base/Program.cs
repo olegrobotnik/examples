129221a345e5d635c7ef4b957c80dbb3c4a391a0
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-02 15:32:11 +0300

    Added code to example006_base project

--- example006_base/Program.cs
+++ example006_base/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 781;
+int b = 2;
+int c = 3333;
+int d = 137;
+int e = 187;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("The maximum number is ");
+Console.Write(max);
